9485baa73dcfbca436d5ae2df011c33aa830953c
wsameer <dev.wsameer@gmail.com>
2024-08-19 22:45:11 -0400

    transactions: adds translations

--- resources/js/Pages/transactions.tsx
+++ resources/js/Pages/transactions.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { PageLayout } from '@/layouts/page-layout';
+import { Head } from '@/Components/seo';
 
 export const TransactionsRoute = () => {
   return (
     <PageLayout>
+      <Head title='Transactions' />
       <p>TransactionsRoute</p>
       <p>List of all transactions</p>
     </PageLayout>
