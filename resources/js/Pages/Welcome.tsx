450e2d589c3c0ec20bd46a3643ce3d21d36146ef
wsameer <dev.wsameer@gmail.com>
2024-08-19 22:46:29 -0400

    adds translations in welcome file

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,39 +1,49 @@
 import React from 'react';
 import { Github } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
+import { Link, useNavigate } from 'react-router-dom';
 
+import { useAuth } from '@/lib/use-auth';
+import { APP_ROUTE, DASHBOARD_ROUTE, LOGIN_ROUTE } from '@/router/routes';
 import { Button } from '@/Components/ui/button';
-import { Link } from 'react-router-dom';
-import { DASHBOARD_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE } from '@/router/routes';
 import { Head } from '@/Components/seo';
 
 export const Welcome = React.memo(() => {
-  const auth = {
-    user: false,
+  const navigate = useNavigate();
+  const { t } = useTranslation('common');
+  const { user } = useAuth();
+
+  const handleStart = () => {
+    if (user) {
+      navigate(APP_ROUTE);
+    } else {
+      navigate(LOGIN_ROUTE);
+    }
   };
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <Head title="Budget Tracker by Sameer" />
+      <Head title={t('welcome.title')} />
       <main className="flex h-screen items-center bg-white">
         <div className="mx-auto max-w-7xl px-4 py-12 text-center sm:px-6 lg:px-8 lg:py-16">
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Budget Tracker
+            {t('welcome.app-name')}
           </h1>
-          <p className="mt-2">
-            Manage your daily expenses and keep track of your budget
+          <p className="mt-2 px-6 sm:px-2">
+            {t('welcome.app-description')}
           </p>
           <div className="mt-8 flex justify-center">
-            {auth.user ? (
+            {user ? (
               <Button variant="default" asChild>
-                <Link to={DASHBOARD_ROUTE}>Dashboard</Link>
+                <Link to={DASHBOARD_ROUTE}>{t('welcome.dashboard')}</Link>
               </Button>
             ) : (
               <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
                 <Button variant="default" asChild>
-                  <Link to={LOGIN_ROUTE}>Log in</Link>
+                  <Link to={LOGIN_ROUTE}>{t('welcome.login')}</Link>
                 </Button>
-                <Button variant="destructive" asChild>
-                  <Link to={REGISTER_ROUTE}>Register</Link>
+                <Button variant="destructive" onClick={handleStart}>
+                  {t('welcome.get-started')}
                 </Button>
                 <a
                   href="https://github.com/wsameer/budget-tracker"
@@ -41,7 +51,7 @@ export const Welcome = React.memo(() => {
                   rel="noreferrer"
                 >
                   <Button variant="outline">
-                    <Github className="mr-2 h-4 w-4" /> Github Repo
+                    <Github className="mr-2 h-4 w-4" /> {t('welcome.github-repo')}
                   </Button>
                 </a>
               </div>
