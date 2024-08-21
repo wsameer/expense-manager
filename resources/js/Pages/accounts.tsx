e56deb5afa4e9f20e46a2890e1fee2e23323815f
wsameer <dev.wsameer@gmail.com>
2024-08-20 22:30:36 -0400

    begin to add features to accounts

--- resources/js/Pages/accounts.tsx
+++ resources/js/Pages/accounts.tsx
@@ -4,7 +4,17 @@ import { PageLayout } from '@/layouts';
 export const AccountsRoute = () => {
   return (
     <PageLayout>
-      <p>AccountsRoute</p>
+      <div className="grid grid-cols-3 gap-3 text-center">
+        <div className="">
+          Assets
+        </div>
+        <div className="">
+          Liabilities
+        </div>
+        <div className="">
+          Total
+        </div>
+      </div>
       <p>a sticky top header showing your assets, liabilities, and total balance </p>
       <p>A table displaying category wise your bank accounts and the total balance left</p>
     </PageLayout>
