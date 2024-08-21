e6b66fb9dce5131c077bc614904f0f3f723d10fd
wsameer <dev.wsameer@gmail.com>
2024-08-20 22:30:17 -0400

    adds label to mobile navigation

--- resources/js/Components/navigation/index.tsx
+++ resources/js/Components/navigation/index.tsx
@@ -21,32 +21,35 @@ export const Navigation = () => {
   const navigate = useNavigate();
 
   const navItems = [
-    { icon: HomeIcon, label: 'Dashboard', path: DASHBOARD_ROUTE },
+    { icon: HomeIcon, label: 'Home', path: DASHBOARD_ROUTE },
     {
       icon: FileTextIcon,
-      label: 'Transactions',
+      label: 'Trans',
       path: TRANSACTIONS_ROUTE,
     },
     { icon: CreditCardIcon, label: 'Accounts', path: ACCOUNTS_ROUTE },
-    { icon: Settings, label: 'Settings', path: SETTINGS_ROUTE },
+    { icon: Settings, label: 'More', path: SETTINGS_ROUTE },
   ].filter(Boolean) as SideNavigationItem[];
 
   if (isMobile) {
     return (
-      <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center h-16">
+      <nav className="fixed bottom-0 left-0 right-0 grid grid-cols-5 gap-3 justify-items-center items-center bg-white border-t border-gray-200 h-16">
         {navItems.slice(0, -1).map((item, index) => (
           <React.Fragment key={item.path}>
             <Button
               variant="ghost"
-              size="icon"
-              className={cn(
+              size="sm"
+              className={cn('p-0',
                 location.pathname === item.path
                   ? 'text-black'
                   : 'text-gray-400',
               )}
               onClick={() => navigate(item.path)}
             >
-              <item.icon className="h-6 w-6" />
+              <div className='flex flex-col items-center justify-center'>
+                <item.icon className="h-5 w-5" />
+                <p className='text-xs'>{item.label}</p>
+              </div>
             </Button>
             {index === 1 && <AddTransaction />}
           </React.Fragment>
@@ -61,7 +64,10 @@ export const Navigation = () => {
           )}
           onClick={() => navigate(navItems[navItems.length - 1].path)}
         >
-          <EllipsisIcon className="h-6 w-6" />
+          <div className='flex flex-col items-center justify-center'>
+            <EllipsisIcon className="h-5 w-5" />
+            <p className='text-xs'>{navItems[navItems.length - 1].label}</p>
+          </div>
         </Button>
       </nav>
     );
