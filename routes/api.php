a29071863ad2dfd26557ab5226c6fb062c3235ef
wsameer <dev.wsameer@gmail.com>
2024-08-26 19:26:31 -0400

    adds API endpoints for accounts

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,4 +14,12 @@ Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
   Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+  // This single line of code is equivalent to writing out all of these routes individually:
+  Route::apiResource('accounts', AccountController::class);
+  // Route::get('/accounts', [AccountController::class, 'index'])->name('accounts.index');
+  // Route::post('/accounts', [AccountController::class, 'store'])->name('accounts.store');
+  // Route::get('/accounts/{account}', [AccountController::class, 'show'])->name('accounts.show');
+  // Route::put('/accounts/{account}', [AccountController::class, 'update'])->name('accounts.update');
+  // Route::delete('/accounts/{account}', [AccountController::class, 'destroy'])->name('accounts.destroy');
 });
