40d536739c33460c4d8304bd1a98a5cfd47223aa
wsameer <dev.wsameer@gmail.com>
2024-09-01 07:32:08 -0400

    moves to display page pattern

--- resources/js/Pages/accounts.tsx
+++ resources/js/Pages/accounts.tsx
@@ -1,43 +1,14 @@
 import React from 'react';
 import { PageLayout } from '@/layouts';
-import { AccountOverviewStat } from '@/features/accounts/components/account-overview-stat';
-import { AccountGroups } from '@/features/accounts/account-groups';
+import { AccountsPage } from '@/features/accounts';
 
 export const AccountsRoute = () => {
-  const statsData = [
-    {
-      id: 1,
-      value: 1000,
-      label: 'Assets',
-    },
-    {
-      id: 2,
-      value: 1000,
-      label: 'Liabilities',
-    },
-    {
-      id: 3,
-      value: 100.01,
-      label: 'Total',
-    },
-  ];
-
   return (
     <PageLayout
       title="Accounts"
       subTitle="Your accounts with its latest balance"
     >
-      <div className="grid grid-cols-1 gap-6">
-        <div className="flex justify-between items-center space-x-3">
-          {statsData.map((item) => (
-            <AccountOverviewStat
-              key={item.id}
-              {...item}
-            />
-          ))}
-        </div>
-        <AccountGroups />
-      </div>
+      <AccountsPage />
     </PageLayout>
   );
 };
