e3c20e5500879fb80f4c6ab7992a3e2190ce46c0
wsameer <dev.wsameer@gmail.com>
2024-09-10 21:50:17 -0400

    reroutes user to login page after registration

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { RegisterForm } from '@/features/auth/register-form';
 import { LOGIN_ROUTE } from '@/router/routes';
@@ -9,14 +9,12 @@ import { useTranslation } from 'react-i18next';
 export const RegisterRoute = () => {
   const navigate = useNavigate();
   const { t } = useTranslation('auth', { keyPrefix: 'forms' });
-  const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get('redirectTo');
 
   return (
     <AuthLayout title={t('sign-up')}>
       <RegisterForm
         onSuccess={() =>
-          navigate(`${redirectTo ? `${redirectTo}` : LOGIN_ROUTE}`, {
+          navigate(LOGIN_ROUTE, {
             replace: true,
           })
         }
