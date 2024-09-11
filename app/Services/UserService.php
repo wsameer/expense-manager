3bc454b13a296654c699b50f4d5d27d12d381ded
wsameer <dev.wsameer@gmail.com>
2024-09-10 21:47:15 -0400

    add a service to add cash account when user registers

---
+++ app/Services/UserService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Account;
+use App\Models\User;
+
+class UserService
+{
+  public function createUser(array $userData)
+  {
+    $user = User::create($userData);
+
+    $this->createDefaultCashAccount($user);
+
+    return $user;
+  }
+
+  private function createDefaultCashAccount(User $user)
+  {
+    return $user->accounts()->create([
+      'name' => 'Cash',
+      'group' => 'CASH',
+      'balance' => '0.00',
+      'description' => 'Default cash account',
+      'payment_account_id' => null,
+      'created_at' => now(),
+      'updated_at' => now(),
+    ]);
+  }
+}
