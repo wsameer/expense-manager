f50edb5fc9a370c1aa5b5e8d7e461c7a1b63c345
wsameer <dev.wsameer@gmail.com>
2024-09-11 19:20:24 -0400

    adds apis for transactions and expenses

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AccountStatController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\ExpenseCategoryController;
+use App\Http\Controllers\ExpenseSubcategoryController;
+use App\Http\Controllers\TransactionController;
+use App\Models\ExpenseCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +20,13 @@ Route::post('/auth/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
   Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-  // This single line of code is equivalent to writing out all of these routes individually:
   Route::apiResource('accounts', AccountController::class);
+
+  Route::apiResource('expense-categories', ExpenseCategoryController::class);
+
+  Route::apiResource('expense-categories.subcategories', ExpenseSubcategoryController::class);
+
+  Route::apiResource('transactions', TransactionController::class);
 
   Route::get('/accounts-stats', [AccountStatController::class, 'index'])->name('accountstat.index');
 });
