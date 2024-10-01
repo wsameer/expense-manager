e3a170b91dcbd121fe36444a53d97b8e1f909371
wsameer <dev.wsameer@gmail.com>
2024-10-01 17:47:58 -0400

    moves code around

---
+++ resources/js/Components/option-selector/index.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@/Components/ui/button';
+
+import React from 'react';
+
+type Props = {
+  onSelect: (value: any) => void;
+  options: { id: number, name: string }[]
+};
+
+export const OptionSelector = React.memo<Props>(({ onSelect, options }) => {
+  return (
+    <div className="grid grid-cols-3 gap-1">
+      {options?.map((option) => (
+        <Button
+          key={option.id}
+          className="px-2 h-14 whitespace-normal text-center leading-3"
+          variant="outline"
+          size="lg"
+          style={{ fontSize: '11px' }}
+          onClick={(e) => {
+            e.preventDefault();
+            onSelect(option);
+          }}
+        >
+          {option.name}
+        </Button>
+      ))}
+    </div >
+  );
+});
+
+OptionSelector.displayName = 'OptionSelector';
---
+++ resources/js/Components/option-selector/sub-option.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SubOption = () => {
+  return (
+    <div>SubOption</div>
+  )
+}
