bbcf477f0dde169a98fa5b1221f2415e877738b7
wsameer <dev.wsameer@gmail.com>
2024-10-08 08:56:08 -0400

    adds utility methods for formatting dates

--- resources/js/utils/index.ts
+++ resources/js/utils/index.ts
@@ -33,3 +33,16 @@ export const cleanString = (input: string): string => {
     .replace(/\s+/g, '')
     .replace(/[^a-z0-9]/gi, '');
 };
+
+export const getFormattedDateTime = (dateTime: Date): string => {
+  const date = new Date(dateTime);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return formattedDate;
+};
