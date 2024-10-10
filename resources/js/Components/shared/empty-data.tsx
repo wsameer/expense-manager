afc17be62a17cc9f484d9bc9edb171c3f395417e
wsameer <dev.wsameer@gmail.com>
2024-10-09 20:53:08 -0400

    adds empty data component

---
+++ resources/js/Components/shared/empty-data.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Ghost } from 'lucide-react'
+
+export const EmptyTransactions = () => {
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center mt-40 text-center">
+      <Ghost className='text-muted-foreground' size={32} />
+      <p className='text-muted-foreground'>No data available</p>
+    </div>
+  )
+}
