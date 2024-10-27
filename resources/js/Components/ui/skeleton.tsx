46bd807cc73aa788d19cd65a2a8a6b1214c7ba5e
wsameer <dev.wsameer@gmail.com>
2024-10-27 11:47:01 -0400

    dark mode styling for skeleton loaders

--- resources/js/Components/ui/skeleton.tsx
+++ resources/js/Components/ui/skeleton.tsx
@@ -7,7 +7,10 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn('animate-pulse rounded-md bg-zinc-200', className)}
+      className={cn(
+        'animate-pulse rounded-md bg-zinc-200 dark:bg-zinc-800',
+        className,
+      )}
       {...props}
     />
   );
