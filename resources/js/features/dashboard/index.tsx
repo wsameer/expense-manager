29fb4458c41b4ff4f9d7899ffbf1770d4c0f1e7e
wsameer <dev.wsameer@gmail.com>
2024-12-15 00:15:45 -0500

    small width on desktop

--- resources/js/features/dashboard/index.tsx
+++ resources/js/features/dashboard/index.tsx
@@ -1,22 +1,28 @@
 import { useState } from 'react';
 import { MonthNavigator } from '@/Components/shared/month-navigator';
 import { ChartContainer } from './components/chart-container';
+import { useResponsive } from '@/hooks';
+import { cn } from '@/utils';
 
 export const DashboardPage = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const { isDesktop } = useResponsive();
 
   const handleMonthChange = (year: number, month: number) => {
     setCurrentDate(new Date(year, month));
   };
 
   return (
-    <div className="mb-4">
+    <div
+      className={cn('grid grid-cols-1 gap-6', {
+        'w-1/3': isDesktop,
+      })}
+    >
       <MonthNavigator
         currentDate={currentDate}
         handleMonthChange={handleMonthChange}
         options={{ timeJump: true }}
       />
-
       <ChartContainer currentDate={currentDate} />
     </div>
   );
