0bc8d7b93a001d6eccc54da627c6bb1d03c4c970
wsameer <dev.wsameer@gmail.com>
2024-12-27 18:57:33 -0500

    updates UI components

--- resources/js/Components/list-group/list-item.tsx
+++ resources/js/Components/list-group/list-item.tsx
@@ -1,27 +1,32 @@
 import { cn } from '@/utils';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { Button } from '../ui/button';
 
-type Variant = 'default' | 'danger'
+type Variant = 'default' | 'danger';
 
 type Props = {
-  icon?: React.ReactNode
-  label: string
-  onClick?: () => void
-  rightElement?: React.ReactNode
-  variant?: Variant
-}
+  icon?: React.ReactNode;
+  label: string;
+  onClick?: () => void;
+  rightElement?: React.ReactNode;
+  variant?: Variant;
+};
 
 const variantStyles: Record<Variant, string> = {
   default: 'bg-white dark:bg-zinc-800 dark:hover:bg-zinc-900',
-  danger: 'bg-red hover:bg-red-100 dark:bg-red-900 dark:hover:bg-red-950'
-}
+  danger: 'bg-red hover:bg-red-100 dark:bg-red-900 dark:hover:bg-red-950',
+};
 
-
-export const ListItem = React.memo<Props>(
-  ({ icon, label, onClick, rightElement, variant = "default" }) => (
+export const ListItem = forwardRef<HTMLButtonElement, Props>(
+  ({ icon, label, onClick, rightElement, variant = 'default' }, ref) => (
     <Button
-      className={cn('flex h-10 items-center justify-between py-1 px-4 border-t border-zinc-200 dark:border-zinc-900 cursor-pointer first:border-t-0', variantStyles[variant])}
+      ref={ref}
+      className={cn(
+        'flex h-10 items-center justify-between py-1 px-4',
+        'border-t border-zinc-200 dark:border-zinc-900',
+        'cursor-pointer first:border-t-0',
+        variantStyles[variant],
+      )}
       onClick={onClick}
       variant="ghost"
       asChild
@@ -34,14 +39,11 @@ export const ListItem = React.memo<Props>(
         >
           {icon ? icon : null}
           <small
-            className={cn(
-              'dark:text-white text-sm font-medium leading-none',
-              {
-                'ml-3': icon,
-                'text-zinc-900': variant === 'default',
-                'text-red-600': variant === 'danger'
-              },
-            )}
+            className={cn('dark:text-white text-sm font-medium leading-none', {
+              'ml-3': icon,
+              'text-zinc-900': variant === 'default',
+              'text-red-600': variant === 'danger',
+            })}
           >
             {label}
           </small>
