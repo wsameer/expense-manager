e8772c46e9c879e2674103459c250a56598d969b
wsameer <dev.wsameer@gmail.com>
2024-12-27 18:58:18 -0500

    adds a feature to upload CSV to import data

--- resources/js/Pages/settings/data-settings.tsx
+++ resources/js/Pages/settings/data-settings.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 import { PageLayout } from '@/layouts';
 import { SETTINGS_ROUTE } from '@/router/routes';
-import { ListGroup } from '@/Components/list-group';
-import { ListItem } from '@/Components/list-group/list-item';
-import { Eraser, FileDown, FileUp } from 'lucide-react';
+import { DataSettings } from '@/features/settings/data-settings';
 
 export const DataSettingsRoute = () => {
   return (
@@ -16,44 +14,7 @@ export const DataSettingsRoute = () => {
         title: 'Settings',
       }}
     >
-      <div
-        id="data-settings"
-        className="grid grid-cols-1 gap-6"
-      >
-        <ListGroup title="Export/Import">
-          <ListItem
-            icon={
-              <FileUp className="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
-            }
-            label="Import from CSV"
-            rightElement={undefined}
-          />
-          <ListItem
-            icon={
-              <FileDown className="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
-            }
-            label="Export to CSV"
-            rightElement={undefined}
-          />
-          <ListItem
-            icon={
-              <FileDown className="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
-            }
-            label="Export to PDF"
-            rightElement={undefined}
-          />
-        </ListGroup>
-        <ListGroup title="Other">
-          <ListItem
-            icon={
-              <Eraser className="h-4 w-4 text-red-600 dark:text-zinc-300" />
-            }
-            label="Erase All Content and Settings"
-            rightElement={undefined}
-            variant='danger'
-          />
-        </ListGroup>
-      </div>
+      <DataSettings />
     </PageLayout>
   );
 };
